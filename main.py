b5389074de9b371e866fdcf6aad9e37dbc7387d7
Gerald Lim <lgjg1994@gmail.com>
2026-04-12 23:30:02 +0800

    Clean old files

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from infotennis-v2!")
-
-
-if __name__ == "__main__":
-    main()
--- test_functions.py
+++
@@ -1,6 +0,0 @@
-from tasks.ingestion.get_atp_calendar import get_atp_results_archive_task
-
-if __name__ == "__main__":
-    # You can specify a year, or leave blank for current year
-    result = get_atp_results_archive_task.fn(2024)  # .fn runs the underlying function synchronously
-    print(result)
